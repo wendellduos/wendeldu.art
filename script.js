02fe32afd36284328645fae1907f15b9f214293a
Wendel <wendellduos@hotmail.com>
2023-12-05 00:34:07 -0300

    refactored script

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 /*  MUSIC PLAYER  */
-let currentTrack = 0;
+// define initial player states
+let current = 0;
 let isPlaying = false;
 
-// get audio files and create track indicator items
-const songList = document.querySelectorAll("audio");
-songList.forEach((song) => {
-  const trackIndicator = document.getElementById("track-indicator");
+// get audio files, create track indicator items and event listeners for when songs end
+const songs = document.querySelectorAll("audio");
+const trackIndicator = document.getElementById("track-indicator");
 
+songs.forEach((song) => {
   trackIndicator.innerHTML += "<div class='track-indicator-item'></div>";
 
-  // autoplay next song, and stop if last
+  // autoplay next song, and stop playback if last
   song.addEventListener("ended", () => {
-    if (currentTrack === songList.length - 1) {
-      songList[currentTrack].currentTime = 0;
+    if (current === songs.length - 1) {
+      songs[current].currentTime = 0;
 
       isPlaying = false;
       changePlayBtnStyle();
@@ -22,25 +23,25 @@ songList.forEach((song) => {
   });
 });
 
+// get all indicator items and highlight the first one
 const trackIndicatorItems = document.querySelectorAll(".track-indicator-item");
 highlightIndicator();
 
+// insert current track's total length when page has fully loaded
 const totalTime = document.getElementById("total-time");
+addEventListener("load", insertTrackInfo());
 
-setTimeout(insertTrackInfo(), 100);
-
-// gets control buttons in DOM and sets a listener for clicks
+// gets control buttons in DOM, sets a listener for clicks and gives task (job) for main function
 const playerControlBtns = document.querySelectorAll("[data-job]");
-
 playerControlBtns.forEach((button) => {
   button.addEventListener("click", () => {
-    let job = button.dataset.job;
-
-    playerControl(job);
+    playerControl(button.dataset.job);
   });
 });
 
+//main function, deals with play, pause, and track skipping
 function playerControl(job) {
+  // canChange variable will be used to ask checkMusicEnds function if buttons should be disabled
   let canChange;
 
   switch (job) {
@@ -48,50 +49,46 @@ function playerControl(job) {
       canChange = checkMusicEnds(job);
 
       if (canChange) {
-        songList[currentTrack].pause();
-        songList[currentTrack].currentTime = 0;
+        songs[current].pause();
+        songs[current].currentTime = 0;
 
-        currentTrack--;
+        current--;
 
         if (isPlaying) {
-          songList[currentTrack].play();
+          songs[current].play();
         }
       }
 
       changeBtnStyles();
       highlightIndicator();
       insertTrackInfo();
-
       break;
 
     case "next":
       canChange = checkMusicEnds(job);
 
       if (canChange) {
-        songList[currentTrack].pause();
-        songList[currentTrack].currentTime = 0;
+        songs[current].pause();
+        songs[current].currentTime = 0;
 
-        currentTrack++;
+        current++;
 
         if (isPlaying) {
-          songList[currentTrack].play();
+          songs[current].play();
         }
       }
 
       changeBtnStyles();
       highlightIndicator();
       insertTrackInfo();
-
       break;
 
     default:
-      if (isPlaying === false) {
-        songList[currentTrack].play();
-
+      if (!isPlaying) {
+        songs[current].play();
         isPlaying = true;
       } else {
-        songList[currentTrack].pause();
-
+        songs[current].pause();
         isPlaying = false;
       }
 
@@ -99,31 +96,41 @@ function playerControl(job) {
   }
 }
 
-// convert song lengths in seconds to minutes and seconds: 0:00
+// insert track's duration in element
+function insertTrackInfo() {
+  totalTime.innerHTML = convertToMinutesAndSeconds(songs[current].duration);
+}
+
+// convert song lengths in seconds to minutes and seconds = 0:00
 function convertToMinutesAndSeconds(time) {
   let minutes = Math.floor(time / 60);
   let seconds = Math.floor(time % 60);
-
+  // forces seconds to appear as two digits
   let fixedSec = String("0" + seconds).slice(-2);
 
   return `${minutes}:${fixedSec}`;
 }
 
-// insert track's duration in element
-function insertTrackInfo() {
-  totalTime.innerHTML = convertToMinutesAndSeconds(songList[currentTrack].duration);
+// highlight current track's position indicator
+function highlightIndicator() {
+  for (i = 0; i < songs.length; i++) {
+    trackIndicatorItems[i].style.backgroundColor = "var(--sand)";
+    if (i === current) {
+      trackIndicatorItems[current].style.backgroundColor = "var(--old-orange)";
+    }
+  }
 }
 
-// check if user reached the last or first songs in list
+// check if user has reached the last or first songs in list
 function checkMusicEnds(direction) {
   if (direction === "previous") {
-    if (currentTrack <= 0) {
+    if (current <= 0) {
       return false;
     } else {
       return true;
     }
   } else if (direction === "next") {
-    if (currentTrack >= songList.length - 1) {
+    if (current >= songs.length - 1) {
       return false;
     } else {
       return true;
@@ -131,22 +138,12 @@ function checkMusicEnds(direction) {
   }
 }
 
-// highlight current track's position indicator
-function highlightIndicator() {
-  for (i = 0; i < songList.length; i++) {
-    trackIndicatorItems[i].style.backgroundColor = "var(--sand)";
-    if (i === currentTrack) {
-      trackIndicatorItems[currentTrack].style.backgroundColor = "var(--old-orange)";
-    }
-  }
-}
-
-// change buttons bg color based on track position
+// change buttons background color based on track position
 function changeBtnStyles() {
-  if (currentTrack === 0) {
+  if (current === 0) {
     playerControlBtns[0].style.backgroundColor = "var(--sand-shadow)";
     playerControlBtns[2].style.backgroundColor = "var(--sand)";
-  } else if (currentTrack === songList.length - 1) {
+  } else if (current === songs.length - 1) {
     playerControlBtns[0].style.backgroundColor = "var(--sand)";
     playerControlBtns[2].style.backgroundColor = "var(--sand-shadow)";
   } else {
@@ -155,6 +152,7 @@ function changeBtnStyles() {
   }
 }
 
+// change play-pause button background color based on play state
 function changePlayBtnStyle() {
   if (isPlaying) {
     playerControlBtns[1].style.backgroundColor = "var(--old-orange)";
